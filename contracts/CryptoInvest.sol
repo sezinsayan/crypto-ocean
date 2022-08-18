ecd2fb0a41510553d1c90ba18fa54265c7ba3d75
Canboraimamoglu <canboramx@gmail.com>
2022-08-18 10:55:13 +0300

    yeni güncelleme

--- contracts/CryptoInvest.sol
+++ contracts/CryptoInvest.sol
@@ -5,14 +5,22 @@ contract CryptoInvest{
     struct Child{
         string name;
         address addr;
+        uint256 idNumber;
+        uint256 balance;
+        uint256 withdrawDate;
     }
 
     mapping(address=>Child) public children;
-    function addChild(address addr,string memory name) public{
+    function addChild(address addr,string memory name,uint idNumber,uint256 withdrawDate) public{
         Child storage _child = children[addr];
+
         require(_child.addr == address(0),"Address is already added!");
+
         _child.name=name;
         _child.addr=addr;
+        _child.idNumber = idNumber;
+        _child.withdrawDate = withdrawDate;
+    
     }
 
     
