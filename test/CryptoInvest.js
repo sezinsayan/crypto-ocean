df5b86fd3368e0ee327235a8a906150112a6f9a1
Canboraimamoglu <canboramx@gmail.com>
2022-08-18 11:17:20 +0300

    addChild testi güncellendi

--- test/CryptoInvest.js
+++ test/CryptoInvest.js
@@ -21,22 +21,29 @@ describe("CryptoInvest", function () {
     it("Right mapping", async function () {
       const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
       const name = "Canbora";
+      const idNumber = 26052484562;
+      const withdrawDate = 1787778000;
 
-      const tx = await crypto.addChild(addr, name);
+      const tx = await crypto.addChild(addr, name,idNumber,withdrawDate);
       const children = await crypto.children(addr);
 
       expect(children.name).equal(name);
       expect(children.addr).equal(addr);
+      expect(children.idNumber).equal(idNumber);
+      expect(children.withdrawDate).equal(withdrawDate);
     })
     it("Should not add more than once", async function () {
       const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
       const name = "Canbora";
+      const idNumber = 26052484562;
+      const withdrawDate = 1787778000;
 
-      await crypto.addChild(addr, name);
-      const promise = crypto.addChild(addr, name);
+
+      await crypto.addChild(addr, name,idNumber,withdrawDate);
+      const promise = crypto.addChild(addr, name,idNumber,withdrawDate);
       
       await expect(promise).eventually.rejectedWith("Address is already added!");
-    
+ 
     })
   })
 })
