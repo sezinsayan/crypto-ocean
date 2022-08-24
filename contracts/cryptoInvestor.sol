895e6777bace2c229f127d8492ec096d727997ea
sezinsayan <sezin.sayan@ozu.edu.tr>
2022-08-24 16:31:17 +0300

    first commit to branch

--- contracts/cryptoInvestor.sol
+++ contracts/cryptoInvestor.sol
@@ -20,7 +20,7 @@ contract CryptoInvestor{
         address addr;
         string phoneNum;
         uint256 balance;
-        
+        address[] childrenList;
          
     }
 
@@ -46,16 +46,29 @@ contract CryptoInvestor{
     }
 
     mapping(address=>Child) public children;
-    function addChild(address addr,string memory name,uint idNumber,uint256 withdrawDate) public{
+    function addChild(address  addr,string memory name,uint idNumber,uint256 withdrawDate) public {
         Child storage _child = children[addr];
+        Parent storage parent = parents[msg.sender];
 
         require(_child.addr == address(0),"Address is already added!");
-
+        require(addr != admin,"This is admin address");
+        
         _child.name=name;
         _child.addr=addr;
         _child.idNumber = idNumber;
         _child.withdrawDate = withdrawDate;
-    
+        parent.childrenList.push(addr);
+    }
+    function isChildofParentList (address addr) public view returns(bool){
+        Child storage child = children[addr];
+        Parent storage parent = parents[msg.sender];
+        
+        for(uint i=0;i<parent.childrenList.length;i++){
+            if(parent.childrenList[i]==child.addr){
+                return true;
+            }
+        }
+        return false;
     }
     function getParents() public view returns(Parent[] memory res){
         res= new Parent[](parentList.length);
@@ -67,6 +80,50 @@ contract CryptoInvestor{
     function removeParent(address addr) public {
         //if parent dies
         delete parents[addr];
+    }
+    function send(address childAddress) payable external {
+        Child storage child = children [childAddress];
+        require(child.addr !=address(0), "The child is not registered");
+        child.balance += msg.value;
+
+
+    } 
+    function deleteChild (address _addr) public {
+        delete(children[_addr]);
+    }
+
+    function getChildrenList (address _addressOfInvestor) public view returns (Child[] memory){
+        uint len = parents[_addressOfInvestor].childrenList.length;
+        Child[] memory _childrenOfInvestor = new Child[](len);
+        for (uint i=0; i<len; i++){
+            _childrenOfInvestor[i] = children[parents[_addressOfInvestor].childrenList[i]];
+        }
+        return _childrenOfInvestor;
+    }
+
+    function withdrawchild(uint amount) public {
+        Child storage child = children [msg.sender];
+        require(msg.sender == child.addr, "Only owner can withdraw funds"); 
+        require(amount <= child.balance, "Insufficient funds");
+
+        child.balance -= amount;
+        payable (msg.sender).transfer(amount);
+    }
+    function withdrawinvestor(uint amount,address payable childAddress) public {
+        Child storage child = children [childAddress];
+        Parent storage parent = parents [msg.sender];
+        require(msg.sender==parent.addr,"Only parents can withdraw funds");
+        bool ischild;
+        for(uint i=0;i<parent.childrenList.length;i++){
+            if(parent.childrenList[i]==child.addr){
+                ischild=true;
+            }
+        }
+        require(ischild,""); 
+        require(amount <= child.balance, "Insufficient funds");
+
+        child.balance -= amount;
+        childAddress.transfer(amount);
     }
 
     function giveRoles(address _addr) public view returns(Role){
--- test/cryptoInvestor.js
+++ test/cryptoInvestor.js
@@ -6,7 +6,13 @@ chai.use(chaiAsPromised);
 
 describe("cryptoInvestor", function () {
     let crypto;
-
+    let admin;
+    let wallets;
+    before(async () => {
+        wallets = await ethers.getSigners();
+        admin = wallets[0].address;
+        console.log("admin: ",admin)
+    })
 
     beforeEach(async () => {
         const CryptoInvestor = await hre.ethers.getContractFactory("CryptoInvestor");
@@ -27,7 +33,7 @@ describe("cryptoInvestor", function () {
             await expect(promise).eventually.rejectedWith("Address is already added!");
           
         })
-        it("Right mapping", async function () {
+        it("Right mapping for Parent", async function () {
             const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
             const first_name = "Sezin";
             const last_name = "Sayan";
@@ -37,8 +43,7 @@ describe("cryptoInvestor", function () {
     
             const tx = await crypto.addInvestor(addr, first_name,last_name,phoneNum,balance);
             const parents = await crypto.parents(addr);
-            const test= await crypto.getParents();
-            console.log("Parent list", test);
+            //plist
         
             expect(parents.first_name).equal(first_name);
             expect(parents.last_name).equal(last_name);
@@ -46,6 +51,29 @@ describe("cryptoInvestor", function () {
             expect(parents.phoneNum).equal(phoneNum);
             expect(parents.balance).equal(balance);
         })
+        it("Right mapping for Child", async function () {
+            const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
+            const name = "Canbora";
+            const idNumber = 26052484562;
+            const withdrawDate = 1787778000;
+      
+            const tx = await crypto.addChild(addr, name,idNumber,withdrawDate);
+            const children = await crypto.children(addr);
+      
+            expect(children.name).equal(name);
+            expect(children.addr).equal(addr);
+            expect(children.idNumber).equal(idNumber);
+            expect(children.withdrawDate).equal(withdrawDate);
+        })
+        it("Should not be admin address",async function () {
+            const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
+            const name = "Canbora";
+            const idNumber = 26052484562;
+            const withdrawDate = 1787778000;
+              
+            const promise = crypto.addChild(admin, name,idNumber,withdrawDate);
+            await expect(promise).revertedWith("This is admin address");
+          })
         it("Parent List",async function (){
             const test= await crypto.getParents();
             console.log("Parent list", test);
@@ -55,6 +83,49 @@ describe("cryptoInvestor", function () {
             const role = await crypto.giveRoles(addr);
             console.log("Role",role);
         
+        })
+        it("address has to be in childlistofparents",async function () {
+            const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
+            const name = "Canbora";
+            const idNumber = 26052484562;
+            const withdrawDate = 1787778000;
+      
+            await crypto.addChild(addr, name,idNumber,withdrawDate);
+            const tr=await crypto.isChildofParentList(addr); 
+            expect(tr).equal(true);
+          
+          })
+    })
+    describe("CheckingDelete", function () {
+        it("Delete process", async function () {
+          const addr = "0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
+          const name = "fatih";
+          const idNumber = 2222220;
+          const withdrawDate = 30;
+          const balance = 21;
+    
+          await crypto.deleteChild(addr);
+          const children = await crypto.children(addr);
+    
+          expect(children.name).equal('');
+          expect(children.idNumber).equal('');
+        })
+      })
+    
+      describe("Checking Fetch Investor's children list", function () {
+        it("Children list fetched correctly.", async function () {
+          const investorAddress=wallets[0].address;
+    
+    
+          await crypto.addChild("0xB0FcDb49CE99482702C1CbBc0A183E570353a707","a",0,1);
+          await crypto.addChild("0x99CF4c4CAE3bA61754Abd22A8de7e8c7ba3C196d","b",0,1);
+          await crypto.addChild("0x1439818DD11823c45fFF01aF0Cd6c50934e27Ac0","c",0,1);
+          const childList = await crypto.getChildrenList(investorAddress);
+          console.log("-----------",childList);
+          expect(childList[0].addr)=="0xB0FcDb49CE99482702C1CbBc0A183E570353a707";
+          expect(childList[1].addr)=="0x99CF4c4CAE3bA61754Abd22A8de7e8c7ba3C196d";
+          expect(childList[2].addr)=="0x1439818DD11823c45fFF01aF0Cd6c50934e27Ac0";
+    
         })
     })
     
